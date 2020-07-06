bfe54d97b41231e13190e3d2920a4f362785f072
carlos <c.andrade86@ymail.com>
2020-07-06 15:05:56 -0500

    demo path created

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+	res.send('Money will always be abundant for you!');
+});
 
-	res.send('Hello World!');
+app.get('/demo', (req, res) => {
+	res.set('X-full-stack', '4life'),
+	res.status(418),
+	res.send('Money will always be abundant for you!');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
